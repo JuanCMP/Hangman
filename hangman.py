7e405d6decb24be2807c4ee9bb43de1407ef3a32
Juan Camilo <camilo.murcia.pimentel@gmail.com>
2021-05-16 15:31:51 -0500

    Update: Architecture changed

--- hangman.py
+++ hangman.py
@@ -24,7 +24,7 @@ def normalization(random_word):
   for index, letter in enumerate(norm_word):
     if letter in normalizations.keys():
       norm_word[index] = normalizations[letter]
-  norm_word = ''.join(split_word)
+  norm_word = ''.join(norm_word)
   return norm_word
 
 def word2lines(norm_word):
@@ -32,7 +32,7 @@ def word2lines(norm_word):
   dotted_word = list(norm_word)
   for index, letter in enumerate(dotted_word):
     dotted_word[index] = '_'
-  return dotted_word, word
+  return dotted_word, norm_word
 
 def lines2word(letter, word):
   '''Put letter in the dotted space if letter is in word.
@@ -47,36 +47,46 @@ def lines2word(letter, word):
       guessed_letter[index] = '_'
   return guessed_letter
 
-def number_lives(live):
-  if live != 0:
-    return print('The letter is wrong, try again \n You have {} lives'.format(live))
-  else:
-    return print('You have lost!')
-
 def run():
   
-  start_option = int(input(print('Are you ready to playing hangman?\n\t1. Yes, I\'m ready\n\t2. I\'m later\n')))
-  assert (start_option == 1) or (start_option == 2), str(start_option) +' does not exist!'
+  try:
+    start_option = int(input(print('Are you ready to playing hangman?\n\t1. Yes, I\'m ready\n\t2. I\'m later\n')))
+  except TypeError:
+    print('You can\'t enter letter, insert a integer option.') 
+  assert (start_option == 1) or (start_option == 2), '{} does not exist!'. format(start_option)
   
-  if start_option == 2:
-    print('Bye!')
-    time.sleep(1)
-    os.system('clear')
-  else:
-    print('Let\'s get start!')
-    for live in range(5, 0, -1):
-      print('You have ' + str(live) + ' lives. Keep safe always!')
-      dotted_word, word = word2lines(normalization(random_name(load_data())))
-      print('The word is:\n', dotted_word)
-      guess_word = input(print('Insert a letter: '))
-      
-      if guess_word in word:
-        lines2word(guess_word, word)
-        
+  
+  while start_option == 1:
+    n_lives = 5
+    print('Number of lives {}'. format(n_lives))
+    dotted_word, word = word2lines(normalization(random_name(load_data())))
+    print('The word to guess is: {}'. format(dotted_word))
+    
+    while n_lives > 0:  
+      letter = input(print('Insert a letter: '))
+      guessed_letter = lines2word(letter, word)
+      print(guessed_letter) # retorne un nuevo tablero colocando la(s) letra(s) en tablero si fue verdadero
+      if letter in list(word):
+        print('Congrat, you have guessed a new letter')  
       else:
-        number_lives(live)
-        if live == 0:
-          break
+        n_lives -= 1
+        print('The letter {} does not exist, you now have {} lives'. format(letter, n_lives))
+      # Bug: El problema de la app es que no me guarda los cambios de las letras que ya se adivinaron.
+    
+    print('The correct word is: {}'. format(word))
+    continue_p = int(input(print('Would you like to continue playing?\n 1. Yes\n 2. No')))
+    if continue_p == 2:
+      start_option = 0
+    else:
+      start_option = 1
+
+
+
+
+
+
+    
+ 
 
 if __name__ == '__main__':
   run()
