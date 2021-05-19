5de37d51e5ab23b64baac95b45667f5b0460e91b
Juan Camilo <camilo.murcia.pimentel@gmail.com>
2021-05-19 10:15:45 -0500

    Update: Fixing bugs

--- hangman.py
+++ hangman.py
@@ -1,92 +1,81 @@
-class hangman:
-  import os
-  import time
-  import random
-  # object = player
+import os
+import time
+import random
+import copy
   
-  def __init__(self, lives = 5):
-    self.lives = lives
-  
-  def load_data(self):
-    with open(data.txt, 'r', encoding='utf-8') as df:
-      words = [word for word in df]
-
-  def random_name(self):
-    '''Pick up just a word randomness from a list of words''' 
-    word = random.choice(self.load_data())
-    sep_word = list(word)
-    return word, sep_word
-
-  def normalization(self):
-    '''Quit acents to the random word'''
-
-    normalizations = {'á':'a','é':'e','í':'i','ó':'o','ú':'u'}
-    word, sep_word_norm = self.random_name()
-    for index, letter in enumerate(sep_word_norm):
-      if letter in normalizations.keys():
-        sep_word_norm[index] = normalizations[letter]
-    return sep_word_norm, word
-
-  def word2lines(self):
-    '''Convert a word to a line of lines'''
-    sep_word_norm, word = self.normalizations()
-    for index, letter in enumerate(sep_word_norm):
-      sep_word_norm[index] = '_'
-    return sep_word_norm, word # [_ _ _], 'oso'
-
-  def lines2word(self, letter):
-    '''Put letter in the dotted space if letter is in word.
-    ex 1: word:'c a m i l a' ---> _ _ _ _ _ _  --> _ a _ _ _ a
-    ex 2: word: ---> 'o s o' ---> o __ o 
-    '''
-    word_line, word = self.word2lines()
-    sep_word, _ = self.normalization()
-    for index, l in enumerate(sep_word):
-      if letter == l:
-        word_line[index] = l
-    return word_line
-
-  def run():   
-    try:
-      start_option = int(input(print('Are you ready to playing hangman?\n\t1. Yes, I\'m ready\n\t2. I\'m later\n')))
-    except TypeError:
-      print('You can\'t enter letter, insert a integer option.') 
-    assert (start_option == 1) or (start_option == 2), '{} does not exist!'. format(start_option)
-    
-    
-    while start_option == 1:
-      n_lives = 5
-      print('Number of lives {}'. format(n_lives))
-      dotted_word, word = word2lines(normalization(random_name(load_data())))
-      print('The word to guess is: {}'. format(dotted_word))
-      
-      while n_lives > 0:  
-        letter = input(print('Insert a letter: '))
-        guessed_letter = lines2word(letter, word)
-        print(guessed_letter) # retorne un nuevo tablero colocando la(s) letra(s) en tablero si fue verdadero
-        if letter in list(word):
-          print('Congrat, you have guessed a new letter')  
-        else:
-          n_lives -= 1
-          print('The letter {} does not exist, you now have {} lives'. format(letter, n_lives))
-        # Bug 1: El problema de la app es que no me guarda los cambios de las letras que ya se adivinaron.
-        # Bug 2. Genera un '_' adicional a la palabra a adivinar
-        # Bug 3. Al inicio me genera un 'None' que precede al input
-      
-      print('The correct word is: {}'. format(word))
-      continue_p = int(input(print('Would you like to continue playing?\n 1. Yes\n 2. No')))
-      if continue_p == 2:
-        start_option = 0
-      else:
-        start_option = 1
-
-
-
-
-
-
-    
  
+def load_data():
+  with open('data.txt', 'r', encoding='utf-8') as df:
+    words = [word for word in df]
+  return words
+
+def random_name():
+  '''Pick up just a single word randomness from a list of words''' 
+  random_word = random.choice(load_data())
+  return random_word  # osó
+
+def normalization():
+  '''Remove accents to the random word'''
+  normalizations = {'á':'a','é':'e','í':'i','ó':'o','ú':'u'}
+  random_word = random_name()
+  for index, letter in enumerate(list(random_word)):
+    if letter in normalizations.keys():
+      list(random_word)[index] = normalizations[letter]  
+  return random_word, list(random_word) # 'osó', 'o s o'
+
+def word2lines():   
+  ''' Convert a word to a line of lines '''
+  norm_word, norm_word_s = normalization() 
+  for idx in range(len(norm_word)):
+    norm_word_s[idx] = '_'
+  return norm_word, norm_word_s # 'oso' --> _ _ _ 
+
+def line2letter(letter, word): # a --> ['o','s','o']
+  ''' Verifica si la letra esta en la palabra y la coloca, si no; deja el cambio anterior '''
+  
+  tidx = [] # letter is in word 
+  fidx = []  # letter is not in word
+  if letter in word:
+    tidx.append(letter)
+  else:
+    fidx.append(letter)
+  return tidx, fidx
+  
+def run():   
+  try:
+    start_option = int(input(print('Are you ready to playing hangman?\n 1. Yes, I\'m ready\n 2. I\'m later\n')))
+  except TypeError:
+    print('You can\'t enter letter, insert a integer option.') 
+  assert (start_option == 1) or (start_option == 2), '{} does not exist!'. format(start_option)
+  
+  while start_option == 1:
+    norm_word, hidden_word = word2lines() #oso, ['_','_','_']
+    print('The word to guess is: {}'. format(hidden_word))
+    n_lives = 5
+    print('Number of lives {}'. format(n_lives))  
+    while n_lives > 0:  
+      letter = input(print('Insert a letter: '))
+      tidx, fidx = line2letter(letter, norm_word)
+      if letter in tidx:
+        print('Good guessed')
+        for i in range(len(tidx)):
+          for j in range(len(norm_word)):
+            if tidx[i] == list(norm_word)[j]:
+              hidden_word[j] = tidx[i]
+          print(hidden_word)
+      else:
+        print('{} is wrong'. format(letter))
+        print('Remember wrong letters {}'.format(fidx))
+        n_lives -= 1
+        print('Lives = {}'.format(n_lives))
+        
+      if n_lives == 0:
+        print('You have lost, the correct word is {}'. format(norm_word))
+        continue_play = int(input(print('Do you play continue to playing?\n 1. Yes\n 2. No')))
+        if continue_play == 1:
+          start_option = 1
+        else:
+          start_option = 0   
 
 if __name__ == '__main__':
   run()
